b2e62e1fe747e7ce941f1e6a5e5032116bb61bac
skhan7776 <skhan7776@gmail.com>
2024-09-12 02:09:44 +0530

    new line

--- Terraform-VPC-learning/main.tf
+++ Terraform-VPC-learning/main.tf
@@ -49,7 +49,7 @@ resource "aws_subnet" "terraform_public_subnet" {
 resource "aws_subnet" "terraform_private_subnet" {
   vpc_id            = aws_vpc.terraform_custom_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "Terraform Private Subnet"
